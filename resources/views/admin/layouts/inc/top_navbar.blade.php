55e2d770b904c523fb56c3d9e101f55662d0b564
Hany Darwish <hany.freestyle4u@gmail.com>
2023-06-21 14:58:23 +0300

    upadte main route

--- resources/views/admin/layouts/inc/top_navbar.blade.php
+++ resources/views/admin/layouts/inc/top_navbar.blade.php
@@ -98,6 +98,51 @@
         </li>
         @endif
 
+        @if(config('adminConfig.top_navbar_user_menu') == true )
+            <li class="nav-item dropdown user-menu">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                    <img src="{{ defAdminAssets('img/user_avatar.jpg') }}" class="user-image img-circle" alt="User Image">
+                </a>
+                <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                    <!-- User image -->
+                    <li class="user-header bg-primary">
+                        <img src="{{ defAdminAssets('img/user_avatar.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                        <p>
+                            {{ Auth::user()->name }}
+                            <small>Member since Nov. 2012</small>
+                        </p>
+                    </li>
+                    <!-- Menu Body -->
+                    <li class="user-body">
+                        <div class="row">
+                            <div class="col-4 text-center">
+                                <a href="#">Followers</a>
+                            </div>
+                            <div class="col-4 text-center">
+                                <a href="#">Sales</a>
+                            </div>
+                            <div class="col-4 text-center">
+                                <a href="#">Friends</a>
+                            </div>
+                        </div>
+                        <!-- /.row -->
+                    </li>
+                    <!-- Menu Footer-->
+                    <li class="user-footer">
+                        <a href="#" class="btn btn-default btn-flat">Profile</a>
+                        <a class="btn btn-default btn-flat float-right" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
+            </li>
+        @endif
+
         @if(config('adminConfig.top_navbar_user_menu') == true)
         <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
@@ -126,6 +171,17 @@
             </div>
         </li>
         @endif
+
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                <i class="fas fa-sign-out-alt"></i>
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </li>
 
         @if(config('adminConfig.top_navbar_fullscreen') == true)
         <li class="nav-item">
