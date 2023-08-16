17c9f9e1e1957a7b1251a98be94e8a1f5f15800c
Hany Darwish <hany.freestyle4u@gmail.com>
2023-08-16 17:47:15 +0300

    Update Seeder

--- database/seeders/config/MetaTagSeeder.php
+++ database/seeders/config/MetaTagSeeder.php
@@ -2,23 +2,27 @@
 
 namespace Database\Seeders\config;
 
+
 use App\Models\admin\config\MetaTag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB ;
 
 class MetaTagSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-        $data = [
-            'cat_id'=>"home",
-        ];
-        $countData =  MetaTag::all()->count();
-        if($countData == '0'){
-            MetaTag::create($data);
-        }
+//        $data = [
+//            'cat_id'=>"home",
+//        ];
+//        $countData =  MetaTag::all()->count();
+//        if($countData == '0'){
+//            MetaTag::create($data);
+//        }
+
+        MetaTag::unguard();
+        $tablePath = public_path('db/meta_tags.sql');
+        DB::unprepared(file_get_contents($tablePath));
     }
 }
